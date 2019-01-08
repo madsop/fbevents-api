3af7cc0ae1be9b4a4091a0e412799b93057e68b6
Mads Opheim <mads.opheim@gmail.com>
2019-01-08 22:29:04 +0100

    Needed to tweak the json a little bit to get it right.

--- gcp.py
+++ gcp.py
@@ -8,9 +8,16 @@ def getFiles():
     blobs = list(bucket.list_blobs())
     return blobs
 
+def clean(pages):
+    pages = pages.replace('b\'', '')
+    pages = pages.replace('\'', '"')
+    pages = pages.replace('}"', '}')
+    return pages
+
 def gets(r):
     filecontent = {}
     for f in getFiles():
         if f.name.endswith(".json"):
-        	filecontent[f.name] = str(f.download_as_string())
+                content = str(f.download_as_string())
+                filecontent[f.name] = clean(content)
     return jsonify(filecontent)
